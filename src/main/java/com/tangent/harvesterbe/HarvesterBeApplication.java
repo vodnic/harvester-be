5a8dafbad86701aadd8b0fa9c6bb23176a6bef0e
Przemek <przemek@node>
2023-08-11 10:41:31 +0200

    Initial commit
    A spring boot app with dependencies:
    - Spring WEB
    - PosrgreSQL Driver
    - Spring Boot Actuator
    - Lombok

---
+++ src/main/java/com/tangent/harvesterbe/HarvesterBeApplication.java
@@ -0,0 +1,13 @@
+package com.tangent.harvesterbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarvesterBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarvesterBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tangent/harvesterbe/HarvesterBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangent.harvesterbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarvesterBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
